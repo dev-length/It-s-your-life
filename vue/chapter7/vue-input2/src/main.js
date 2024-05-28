f740684fb36961836ab69f8fd63326beacec9eca
length <dlagudrb78@gmail.com>
2024-05-28 18:00:40 +0900

    0528 vue 커밋

---
+++ vue/chapter7/vue-input2/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// import CheckboxItem from './components/CheckboxItem.vue';
+// createApp(App).component('CheckboxItem', CheckboxItem).mount('#app');
+
+createApp(App).mount('#app');
---
+++ vue/chapter7/vue-todolist/src/main.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// import CheckboxItem from './components/CheckboxItem.vue';
+// createApp(App).component('CheckboxItem', CheckboxItem).mount('#app');
+
+createApp(App).mount('#app');
