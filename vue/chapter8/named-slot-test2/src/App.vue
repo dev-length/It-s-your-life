14cf401c7d322509b85caeeb35af1bcd9538d8e8
length <dlagudrb78@gmail.com>
2024-05-30 17:46:48 +0900

    0530 커밋

---
+++ vue/chapter8/named-slot-test2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <SlotTest />
+    <NamedSlotTest />
+    <ScopedSlotTest />
+  </div>
+</template>
+<script>
+import SlotTest from './components/SlotTest.vue';
+import NamedSlotTest from './components/NamedSlotTest.vue';
+import ScopedSlotTest from './components/ScopedSlotTest.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SlotTest,
+    NamedSlotTest,
+    ScopedSlotTest,
+  },
+};
+</script>
